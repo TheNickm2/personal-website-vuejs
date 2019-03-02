9453e56c15a912aabbc6c66d3547dcc6999432ab
Nick Mykkanen <th3nickm2@gmail.com>
2019-03-01 23:30:47 -0600

    Update header style and scripts

--- src/js/script.js
+++ src/js/script.js
@@ -1,14 +1,23 @@
 $(document).ready(() => {
-    $('a[href*=#]:not([href=#])').click(function () {
-        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
-            var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-            if (target.length) {
-                $('html,body').animate({
-                    scrollTop: target.offset().top
-                }, 500);
-                return false;
-            }
+    $('a').on('click', function (event) {
+        if (this.hash !== '') {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 500, function () {
+                window.location.hash = hash;
+            });
+        }
+    });
+
+    const headerElement = document.querySelector('header');
+    $(window).scroll(() => {
+        if ($(window).scrollTop() > $(headerElement).height()) {
+            $(headerElement).addClass('small-header');
+        }
+        else {
+            $(headerElement).removeClass('small-header');
         }
     });
 });
