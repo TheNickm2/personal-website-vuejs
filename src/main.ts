b701745c1dc867817254c7ebee52d058f23f57e5
BuildTools <25287325+nickmykkanen@users.noreply.github.com>
2019-11-09 22:31:16 -0600

    add about section

--- src/main.ts
+++ src/main.ts
@@ -13,11 +13,11 @@ import BootstrapVue from 'bootstrap-vue';
 // FontAwesome Icons
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faStar, faEnvelope, faPhone } from '@fortawesome/free-solid-svg-icons';
-import { faLinkedin, faGitlab, faGithub, faFacebookF, faTelegramPlane, faHtml5, faCss3, faSass, faJs, faWordpressSimple, faReact, faVuejs, faNodeJs, faNpm, faGulp, faYarn, faGit, faJira, faAdobe, faLinux } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedinIn, faGitlab, faGithub, faFacebookF, faTelegramPlane, faHtml5, faCss3, faSass, faJs, faWordpressSimple, faReact, faVuejs, faNodeJs, faNpm, faGulp, faYarn, faGit, faJira, faAdobe, faLinux } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
 library.add(faStar, faEnvelope, faPhone);
-library.add(faLinkedin, faGitlab, faGithub, faFacebookF, faTelegramPlane, faHtml5, faCss3, faSass, faJs, faWordpressSimple, faReact, faVuejs, faNodeJs, faNpm, faGulp, faYarn, faGit, faJira, faAdobe, faLinux);
+library.add(faLinkedinIn, faGitlab, faGithub, faFacebookF, faTelegramPlane, faHtml5, faCss3, faSass, faJs, faWordpressSimple, faReact, faVuejs, faNodeJs, faNpm, faGulp, faYarn, faGit, faJira, faAdobe, faLinux);
 
 Vue.component('font-awesome-icon', FontAwesomeIcon);
 
