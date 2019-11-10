5ff493aabf2c0d79c523aa4ff2638a86b944f12c
BuildTools <25287325+nickmykkanen@users.noreply.github.com>
2019-11-09 23:24:44 -0600

    add footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,14 +1,39 @@
 <template>
-    <footer></footer>
+    <footer class="bg-dark">
+        <div class="copyright text-muted text-center">
+            &copy; {{year}} Nick Mykkanen. <a href="https://gitlab.com/nickmykkanen/personal-website" target="_blank" rel="noreferrer">Fork me on <font-awesome-icon :icon="['fab', 'gitlab']"/> GitLab</a>
+        </div>
+    </footer>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
-export default Vue.extend({
-    
-})
+    import Vue from 'vue';
+    export default Vue.extend({
+        data () {
+            return {
+                year: ""
+            }
+        },
+        mounted () {
+            const year = new Date().getFullYear();
+            if (year > 2019) {
+                this.year = `2019 - ${year}`;
+            }
+            else {
+                this.year = "2019";
+            }
+        }
+    });
 </script>
 
 <style lang="scss">
-
+    footer {
+        padding: 1rem;
+        a {
+            color: #fff;
+            &:hover {
+                color: #fff;
+            }
+        }
+    }
 </style>
