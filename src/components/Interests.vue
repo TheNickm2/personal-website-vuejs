885d614b0d0ae447c6c579d590cc31205a990ca9
BuildTools <25287325+nickmykkanen@users.noreply.github.com>
2019-11-09 23:14:49 -0600

    add interests

--- src/components/Interests.vue
+++ src/components/Interests.vue
@@ -1,12 +1,24 @@
 <template>
-    <section></section>
+    <section class="interests" id="interests">
+        <div class="container">
+            <div class="section-title">
+                <h2>
+                    <span>Other </span>
+                    <span>Interests</span>
+                </h2>
+            </div>
+            <div class="section-info">
+                <p>Outside of work, I'm an all-around tech enthusiast. I love experimenting with new technologies and devices. When I'm not enjoying the tech lifestyle, I like to play games with my friends and family. On a night in, I like to watch action and comedy movies, and am particularly interested in superhero movies.</p>
+            </div>
+        </div>
+    </section>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
-export default Vue.extend({
-    
-})
+    import Vue from 'vue';
+    export default Vue.extend({
+        
+    });
 </script>
 
 <style lang="scss">
